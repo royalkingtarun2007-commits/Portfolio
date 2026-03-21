29d7eb7625527fe1fc027cf21abb5752c866ce57
Tarun Nani <royalkingtarun.2007@gmail.com>
2026-03-21 08:30:53 +0530

    Delete js directory

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-export function initAnimations() {
-  const io = new IntersectionObserver(entries => {
-    entries.forEach(e => {
-      if (e.isIntersecting) e.target.classList.add('show');
-    });
-  }, { threshold: 0.12 });
-
-  document.querySelectorAll('.fade-in').forEach(el => io.observe(el));
-}
